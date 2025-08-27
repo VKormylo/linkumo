87088e5317340c764d403bb7d618b0fdc21a15a2
Sviatoslav <samoliuk.sviatoslav@gmail.com>
2025-08-27 13:29:35 +0300

    chore: initialize vitest

---
+++ linkumo-client/vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import svgrPlugin from 'vite-plugin-svgr'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths(), svgrPlugin()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+  },
+})
