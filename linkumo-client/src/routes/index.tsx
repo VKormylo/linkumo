66c792759ac931a5d68ed992970e40a35d5b114b
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-08-29 20:45:30 +0300

    feat: implemented Auth page markup

--- linkumo-client/src/routes/index.tsx
+++ linkumo-client/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
+import Auth from '~/pages/auth/Auth'
 import ComponentsPreview from '~/pages/components-preview-page/ComponentsPreview'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/componentsPreview" element={<ComponentsPreview />} />
+      <Route path="/auth" element={<Auth />} />
     </Routes>
   )
 }
