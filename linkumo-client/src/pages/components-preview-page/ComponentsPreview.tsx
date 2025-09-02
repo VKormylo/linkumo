ce30f8ba35630a87bd90518c19c23043165687e7
sviatoslav06 <101927757+sviatoslav06@users.noreply.github.com>
2025-09-02 20:21:39 +0300

    (SP: ) Implemented custom button component (#5)
    * feat: implemented custom button component
    
    * feat: added startIcon and endIcon to button component

--- linkumo-client/src/pages/components-preview-page/ComponentsPreview.tsx
+++ linkumo-client/src/pages/components-preview-page/ComponentsPreview.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
+import Button from '~/components/button/Button'
 import Input from '~/components/input/Input'
+import AddIcon from '~/assets/icons/add.svg?react'
 
 const ComponentsPreview = () => {
   const [inputValue, setInputValue] = useState('')
@@ -9,7 +11,7 @@ const ComponentsPreview = () => {
       <h1 className="mt-10 text-2xl text-primary-800">
         Welcome to components page
       </h1>
-      <div className="m-10 flex-col gap-10">
+      <div className="m-10 flex-col">
         <Input
           label="Email"
           value={inputValue}
@@ -18,6 +20,24 @@ const ComponentsPreview = () => {
           onChange={setInputValue}
           className="w-[390px]"
         />
+        <div className="flex-row gap-[10px]">
+          <Button variant="primary" className="mt-10 w-xs">
+            Sign Up
+          </Button>
+          <Button variant="primary-outlined" className="mt-10 w-xs">
+            Sign Up
+          </Button>
+          <Button variant="secondary-outlined" className="mt-10 w-xs">
+            Sign Up
+          </Button>
+          <Button
+            variant="primary"
+            className="mt-10 w-xs"
+            startIcon={<AddIcon />}
+          >
+            Add
+          </Button>
+        </div>
       </div>
     </div>
   )
