37a902b9d4304380111672417072c3432ff980bd
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-09-11 22:19:07 +0300

    feat: created auth and user zod schemas

---
+++ linkumo-client/src/schemas/auth.ts
@@ -0,0 +1,8 @@
+import z from 'zod'
+import { UserSchema } from './user'
+
+export const UserSignupSchema = UserSchema.omit({ rememberMe: true })
+export const UserLoginSchema = UserSchema.omit({ name: true })
+
+export type UserSignup = z.infer<typeof UserSignupSchema>
+export type UserLogin = z.infer<typeof UserLoginSchema>
